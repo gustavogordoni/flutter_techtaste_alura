3dbd90cc14ff6e7f00e7ce5e198ad5f7117dd2ea
gustavogordoni <gustavogordoni1@gmail.com>
2025-04-15 14:49:56 -0300

    Aula 02 - Transforme dados em experiência real! Construa a Home e exiba os restaurantes

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_techtaste/data/restaurant_data.dart';
 import 'package:flutter_techtaste/ui/_core/app_theme.dart';
 import 'package:flutter_techtaste/ui/splash/splash_screen.dart';
+import 'package:provider/provider.dart';
 
-void main(){
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  RestaurantData restaurantData = RestaurantData();
+  await restaurantData.getRestaurants();
+
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) {
+            return restaurantData;
+          },
+        ),
+      ],
+      child: MyApp(),
+    ),    
+  );
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  Widget build(BuildContext context){
-    return MaterialApp(
-      theme: AppTheme.appTheme,
-      home: SplashScreen(),
-    );
+  Widget build(BuildContext context) {
+    return MaterialApp(theme: AppTheme.appTheme, home: SplashScreen());
   }
 }
